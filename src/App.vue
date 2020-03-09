0f0c3d62ed0b5b0ee93c112d05265af95d70b41b
Wilson Redd <wilsonredd@gmail.com>
2020-03-09 15:17:43 -0600

    Created responsive nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,132 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<div id="app">
+    <div id="menu">
+        <div id="brand">
+            <router-link to="/">
+                <img src="/images/logo.png">
+            </router-link>
+        </div>
+        <div id="nav">
+
+            <router-link to="/portfolio">
+                <div class="menu-item portfolio-link">
+                    <p>PORTFOLIO</p>
+                </div>
+            </router-link>
+
+            <router-link to="/blog">
+                <div class="menu-item blog-link">
+                    <p>BLOG</p>
+                </div>
+            </router-link>
+
+        </div>
     </div>
-    <router-view/>
-  </div>
+    <router-view />
+    <!-- Footer -->
+    <div class="footer">
+        <a href="https://github.com/wilsonredd/personal-portfolio.git" target="_blank">
+            https://github.com/wilsonredd/personal-portfolio.git </a>
+    </div>
+    <!-- Footer -->
+</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', sans-serif;
+}
+
+#menu {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 5px;
+    grid-template-areas: "brand nav";
+    padding: 20px 5px 20px 5px;
+    margin: 0px 5px 0px 5px;
+    border-bottom: 1px solid #e3e3e3;
+}
+
+#brand {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#brand img {
+    height: 100px;
+}
+
+ul {
+    list-style-type: none;
 }
 
 #nav {
-  padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#nav a:hover {
+    text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.menu-item {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.1em;
+    font-family: 'Raleway', sans-serif;
+}
+
+.router-link-active {
+    text-decoration: none !important;
+}
+
+.menu-item p {
+    margin: 0px;
+    color: #2F4F4F;
+}
+
+.blog-link {
+    margin-top: 20px;
+}
+
+.footer {
+    padding: 5px;
+    text-align: center;
+}
+
+.footer>a {
+    color: #2F4F4F;
+    text-decoration: none;
+}
+
+@media only screen and (min-width: 901px) {
+    #menu {
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-areas: "none brand nav";
+    }
+
+    #brand {
+        grid-area: brand;
+    }
+
+    #nav {
+        grid-area: nav;
+        flex-direction: row;
+    }
+
+    .menu-item {
+        font-size: 1.5em;
+    }
+
+    .blog-link {
+        margin: 0;
+        margin-left: 50px;
+    }
 }
 </style>
